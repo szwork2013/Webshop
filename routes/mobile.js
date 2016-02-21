ad438834a52d4afef417a6417b6b8107f25cc1a0
itellyouu <827433149@qq.com>
2016-02-21 14:20:19 +0800

    add bootstrap

--- routes/mobile.js
+++ routes/mobile.js
@@ -1,6 +1,6 @@
 module.exports = function (app){
     // mobile index
     app.get('/mobile/', function (req, res, next){
-        res.send('hello mobile!');
+        res.render('mobile/index', {title : '首页-Shop'});
     })
 }
