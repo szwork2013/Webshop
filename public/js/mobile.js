bd29bea9e41b616e9df6503457950e92ae1a4975
itellyouu <827433149@qq.com>
2016-02-22 14:40:33 +0800

    add mobile index page login reg... remaining cart

--- public/js/mobile.js
+++ public/js/mobile.js
@@ -1,12 +1,42 @@
 $(function(){
-	// $(".header-nav").on('click',function(){
-		
-	// 	if($("header").hasClass("active")){
-	// 		$("header").removeClass('active');
-	// 		$(".nav-left").removeClass('active');
-	// 	}else{
-	// 		$("header").addClass('active');
-	// 		$(".nav-left").addClass('active');
-	// 	}
-	// })
+	$(".header-nav").on('click',function(){
+        if($("#mobile-main").hasClass("active")){
+            $("html,body").css({
+                "overflow-y": ""
+            })
+            $(".nav-left").css({
+                "width":"0%"
+            })
+            $("#mobile-main").removeClass("active");
+        }else{
+            $("html,body").css({
+                "overflow": "hidden"
+            })
+            $(".nav-left").css({
+                "width":"60%"
+            })
+            $("#mobile-main").addClass("active");
+            $("body").height( $(window).height() );
+        }
+	})
+    
+    $.get("/commodity",function(data){
+        $.each(data,function(i,j){
+            var html = '<div class="col-xs-6"><div class="shopcom-img"><img src="'+j.imgsrc+'" class="img-responsive" alt=""><a href="/mobile/page/'+j._id+'" class="shopcom-name">'+j.name+'</a></div></div>';
+            $(".shopcom-m").append(html);
+        })
+    })
+    
+    $(".pro-btn").on('click', function(){
+        var data = {
+            name: $('.sname').html(),
+            price: $('.sprice').html(),
+            num: $('.snum').val()
+        }
+        $.post('/addcart',data,function(success){
+            alert(success);
+            location.href = '/cart';
+        })
+    })
+    
 })
--- routes/mobile.js
+++ routes/mobile.js
@@ -1,6 +1,41 @@
+var dbcommodity = require('../model/commodity');
+var dbscart = require('../model/scart');
 module.exports = function (app){
     // mobile index
     app.get('/mobile/', function (req, res, next){
         res.render('mobile/index', {title : '首页-Shop',css : 'index'});
+    })
+    
+    // mobile page
+    app.get('/mobile/page/:id', function (req, res, next){
+        dbcommodity.find({_id:req.params.id},function (err,data){
+            if(err){
+                console.log(err);
+            }
+             res.render('mobile/page', {title: data[0].name, css:'page',data:data });
+        })
+    })
+    
+    // mobile cart
+    app.get('/mobile/cart', function (req, res, next){
+        if(!req.session.user){
+            res.redirect('/login');
+        }
+        dbscart.find({user:req.session.user}, function (err,data){
+            if(err){
+                console.log(err);
+            }
+            res.render('mobile/cart', {title: '购物车', css:'cart', data:data});
+        })
+    })
+    
+    // mobile login
+    app.get('/mobile/login', function (req, res, next){
+        res.render('mobile/login',{title: 'login',css: 'login'})
+    })
+    
+    // mobile reg
+    app.get('/mobile/reg', function (req, res, next){
+        res.render('mobile/reg',{title: 'reg', css:'reg'})
     })
 }
